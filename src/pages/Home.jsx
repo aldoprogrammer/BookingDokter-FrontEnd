6e9c681d466f3ae3e0afddb4fc8fe40f9a56218f
aldoprogrammer <aldobesma@gmail.com>
2024-01-19 14:15:46 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
+import videoIcon from '../assets/images/video-icon.png'
+import avatarIcon from '../assets/images/avatar-icon.png'
+import featureImg from '../assets/images/feature-img.png'
 import { Link } from 'react-router-dom'
 import { BsArrowRight} from 'react-icons/bs'
 import About from '../components/About/About'
@@ -214,6 +217,84 @@ const Home = () => {
               </p>
             </div>
             <ServicesList />
+          </div>
+        </section>
+
+        {/* feature sectoin */}
+        <section>
+          <div className="container">
+            <div className="flex items-center justify-between flex-col
+            lg:flex-row">
+              {/* feature content */}
+              <div className='xl:w-[670]'>
+                <h2 className="heading">
+                  Get virtual treatement <br /> anytime.
+                </h2>
+                <ul className="pl-4">
+                  <li className="text__para">
+                    1.Schedule the appoinemnt directly
+                  </li>
+                  <li className="text__para">
+                    2.Know your healty standarts
+                  </li>
+                  <li className="text__para">
+                    3.Be ready to live like a healthy life
+                  </li>
+                </ul>
+                <Link to='/'>
+                  <button className="btn">Learn More</button>
+                </Link>
+              </div>
+              {/* feature img */}
+              <div className="relative z-10 xl:w-[770px] flex justify-end
+              mt-[50px] lg:mt-0">
+                <img 
+                  src={featureImg}
+                  alt=''
+                  className='w-3/4'
+                />
+                <div className='w-[150px] lg:w-[248px] bg-white absolute
+                bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20
+                p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]'>
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-[6px]
+                    lg:gap-3">
+                      <p className='text-[10px] leading-[10px] lg:text-[14px]
+                      lg:leading-5 text-headingColor font-[600]'>
+                        Tue, 24
+                      </p>  
+                      <p className='text-[10px] leading-[10px] lg:text-[14px]
+                      lg:leading-5 text-textColor font-[600]'>
+                        10:00 AM
+                      </p>  
+                    </div>
+                    <span className="w-5 h-5 lg:w-[34px] lg:h-[34px]
+                    flex items-center justify-center bg-yellowColor py-1
+                    px-[6px] lg:py-3 lg:px-[9px]">
+                      <img 
+                        src={videoIcon}
+                        alt=''
+                      />
+                    </span>
+                  </div>
+
+                  <div className='w-[65px] lg:w-[96px] bg-[#ccf0f3] py-1
+                  px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px]
+                  lg:leading-4 text-irisBlueColor font-[500]
+                  mt-2 lg:mt-4 rounded-full'>
+                    Consultation
+                  </div>
+                  <div className="flex items-center gap-[6px]
+                  lg:gap-[10px] mt-2 lg:mt-[18px]">
+                    <img src={avatarIcon} alt='' />
+                    <h4 className='text-[10px] leading-3 lg:text-[16px]
+                    lg:leading-[22px] font-[700] text-headingColor'>
+                      Aldo Lata SOba
+                    </h4>
+                  </div>
+                </div>  
+              </div>
+            </div>
           </div>
         </section>
     </>
