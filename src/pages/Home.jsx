47496d1fb79d77ddc4b87fc0add8d3505627ac0b
aldoprogrammer <aldobesma@gmail.com>
2024-01-19 09:54:30 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import heroImg01 from '../assets/images/hero-img01.png'
 import heroImg02 from '../assets/images/hero-img02.png'
 import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
+import icon02 from '../assets/images/icon02.png'
+import icon03 from '../assets/images/icon03.png'
+import { Link } from 'react-router-dom'
+import { BsArrowRight} from 'react-icons/bs'
 
 const Home = () => {
   return (
@@ -101,6 +105,90 @@ const Home = () => {
                   font-[700] text-center'>
                     Find a Doctor
                   </h2>
+                  <p className='text-[16px] leading-7 text-textColor
+                  font-[400] mt-4 text-center'>
+                    World-Class care for everyone. Our health system offers unmatched,
+                    expert health care. From the lab to clinic.
+                  </p>
+
+                  <Link 
+                    to='/doctors'
+                    className='w-[44px] h-[44px] rounded-full border
+                    border-solid border-[#181A1E] mt-[30px] mx-auto
+                    flex items-center justify-center group hover:bg-primaryColor
+                    hover:border-none'
+                  >
+                    <BsArrowRight 
+                      className='group-hover:text-white
+                      w-6 h-5'
+                    />
+                  </Link>
+                </div>
+              </div>
+
+              <div className='py-[30px] px-5'>
+                <div className="flex items-center justify-center">
+                  <img 
+                    src={icon02}
+                    alt=""
+                  />
+                </div>
+                <div className='mt-[30px]'>
+                  <h2 className='text-[26px] leading-9 text-headingColor
+                  font-[700] text-center'>
+                    Find a Location
+                  </h2>
+                  <p className='text-[16px] leading-7 text-textColor
+                  font-[400] mt-4 text-center'>
+                    World-Class care for everyone. Our health system offers unmatched,
+                    expert health care. From the lab to clinic.
+                  </p>
+
+                  <Link 
+                    to='/doctors'
+                    className='w-[44px] h-[44px] rounded-full border
+                    border-solid border-[#181A1E] mt-[30px] mx-auto
+                    flex items-center justify-center group hover:bg-primaryColor
+                    hover:border-none'
+                  >
+                    <BsArrowRight 
+                      className='group-hover:text-white
+                      w-6 h-5'
+                    />
+                  </Link>
+                </div>
+              </div>
+
+              <div className='py-[30px] px-5'>
+                <div className="flex items-center justify-center">
+                  <img 
+                    src={icon03}
+                    alt=""
+                  />
+                </div>
+                <div className='mt-[30px]'>
+                  <h2 className='text-[26px] leading-9 text-headingColor
+                  font-[700] text-center'>
+                    Book Appointment
+                  </h2>
+                  <p className='text-[16px] leading-7 text-textColor
+                  font-[400] mt-4 text-center'>
+                    World-Class care for everyone. Our health system offers unmatched,
+                    expert health care. From the lab to clinic.
+                  </p>
+
+                  <Link 
+                    to='/doctors'
+                    className='w-[44px] h-[44px] rounded-full border
+                    border-solid border-[#181A1E] mt-[30px] mx-auto
+                    flex items-center justify-center group hover:bg-primaryColor
+                    hover:border-none'
+                  >
+                    <BsArrowRight 
+                      className='group-hover:text-white
+                      w-6 h-5'
+                    />
+                  </Link>
                 </div>
               </div>
             </div>
