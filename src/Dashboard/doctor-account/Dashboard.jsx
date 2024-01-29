4169bfc33699f9c105274c86bc08c4bc68f3ec14
aldoprogrammer <aldobesma@gmail.com>
2024-01-29 13:44:54 +0700

    udpate

--- src/Dashboard/doctor-account/Dashboard.jsx
+++ src/Dashboard/doctor-account/Dashboard.jsx
@@ -5,6 +5,7 @@ import { BASE_URL } from '../../config.js'
 import Tabs from './Tabs'
 import { useState } from 'react'
 import { FaExclamation, FaExclamationCircle } from "react-icons/fa";
+import starIcon from '../../assets/images/Star.png'
 
 
 
@@ -46,13 +47,45 @@ const Dashboard = () => {
                 {
                   tab === 'overview' && (
                     <div>
-                      <div className="flex items-center gap-4 mb-10">
+                      <div className="flex items-center gap-4 mb-10
+                      ">
                         <figure className='max-w-[200px] max-h-[200px]'>
                           <img 
                             src={data?.photo} 
                             alt=''
                             className='w-full' />
                         </figure>
+
+                        <div className='flex flex-col lg:gap-1'>
+                          <span className="bg-[#ccf0f3] text-irisBlueColor py-1
+                          px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-base
+                          lg:leading-6 font-semibold">
+                            Surgeon
+                          </span>
+                          <h3 className='text-[22px] leading-9 font-bold
+                        text-headingColor'>
+                          Aldo Lata SOba
+                        </h3>
+
+                        <div className="flex items-center gap-[6px]">
+                          <span className="flex items-center gap-[6px] text-headingColor
+                          text-sm leading-5 lg:leading-6 lg:text-base font-semibold">
+                            <img 
+                              src={starIcon}
+                              alt=''
+                              />
+                              4.5
+                          </span>
+                          <span className="gap-[6px] text-textColor
+                          text-sm leading-5 lg:leading-6 lg:text-base font-semibold">
+                              (233)
+                          </span>
+                        </div>
+
+                        <p className="text__para font-[15px] lg:max-w-[390px] leading-6">
+                          doctor bio
+                        </p>
+                        </div>                       
                       </div>
                     </div>
                   )
