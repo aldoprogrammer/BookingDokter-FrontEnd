465f060157f342aec74d86af46da081265aa385b
aldoprogrammer <aldobesma@gmail.com>
2024-01-29 19:57:42 +0700

    udpate

--- src/Dashboard/doctor-account/Profile.jsx
+++ src/Dashboard/doctor-account/Profile.jsx
@@ -19,11 +19,18 @@ const Profile = () => {
         timeSlots: [
             {day: '', startingTime: '', endingTime: ''}
         ],
+        about: '',
+        photo: '',
     })
 
     const handleInputChange = (e) => {
         setFormData({...formData, [e.target.name]:e.target.value})
     }
+
+    const handleFileInputChange = (e) => {
+
+    }
+
 
 
   return (
@@ -351,6 +358,61 @@ const Profile = () => {
                     Add TimeSlotes
                 </button>
             </div>
+
+            <div className="mb-5">
+                <p className="form__label">
+                    About
+                </p>
+                <textarea 
+                    name="about" 
+                    rows={5} 
+                    value={formData.about}
+                    placeholder="Write your bio"    
+                    onChange={handleInputChange}
+                    className='form__input'
+                    >
+                </textarea>
+            </div>
+
+            <div className="mb-5 flex items-center gap-3">
+            {formData.photo && ( 
+              <figure className='w-[60px] h-[60px] rounded-full border-2
+              border-solid border-primaryColor flex items-center justify-center'>
+                <img 
+                  src={formData.photo}
+                  alt=""
+                  className='w-full 
+                  rounded-full'
+                />
+              </figure>)}
+
+              <div className="relative w-[130px] h-[50px]">
+                <input 
+                  type="file" 
+                  name="photo"
+                  onChange={handleFileInputChange} 
+                  id="customFile"
+                  accept='.jpg, .png'
+                  className='absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer'
+                  />
+                  <label 
+                    htmlFor="customFile"
+                    className='absolute top-0 left-0 w-full h-full flex items-center
+                    px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden
+                    bg-[#0066ff46] text-headingColor font-semibold
+                    rounded-lg truncate cursor-pointer'
+                  >
+                    Upload Photo
+                  </label>
+              </div>
+            </div>  
+
+            <div className="mt-7">
+                <button className='bg-primaryColor text-white text-[18px] leading-[30px]
+                w-full py-3 px-3 rounded-lg'>
+                    Update Profile
+                </button>    
+            </div>      
         </form>
     </div>
   )
