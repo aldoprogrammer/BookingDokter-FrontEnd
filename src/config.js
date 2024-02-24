b0f76074e6ac239d5e6ce6ad72789384d2ba55d1
aldoprogrammer <aldobesma@gmail.com>
2024-02-24 16:59:06 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000/api/v1'
+export const BASE_URL = 'https://bookingdokter-backend.onrender.com/api/v1'
 export const token = localStorage.getItem('token');
